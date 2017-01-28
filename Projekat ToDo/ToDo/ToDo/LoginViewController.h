d5e615b848acdbbd750f065f9b2c2d2f7c3caaec
Biljana Jerkovic <biljajerkovic@gmail.com>
2017-01-28 11:59:02 +0100

    Adding projects

---
+++ Projekat ToDo/ToDo/ToDo/LoginViewController.h
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.h
+//  ToDo
+//
+//  Created by Administrator on 14/12/2016.
+//  Copyright © 2016 Djuro Alfirevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *usernameImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *confirmButtonLeadingConstraint;
+@property(strong,nonatomic) NSDictionary *attributes;
+
+- (void)configurePlaceholders;
+- (void)configureTextField:(UITextField *)textField;
+@end
