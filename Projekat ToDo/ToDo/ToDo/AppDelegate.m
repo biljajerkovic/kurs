7533b25f2f9909865cef063ee9858fde72c3a69e
Biljana Jerkovic <biljajerkovic@gmail.com>
2017-02-25 12:18:58 +0100

    DataManager added CLocationManager added CPX added CLGeocoder added

--- Projekat ToDo/ToDo/ToDo/AppDelegate.m
+++ Projekat ToDo/ToDo/ToDo/AppDelegate.m
@@ -7,13 +7,46 @@
 //
 
 #import "AppDelegate.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface AppDelegate ()
+@interface AppDelegate() <CLLocationManagerDelegate>
+@property (strong, nonatomic) CLLocationManager *locationManager;
 @end
 
 @implementation AppDelegate
 
+#pragma mark - Private API
+
+- (void)configureLocationManager {
+    
+    if (![CLLocationManager locationServicesEnabled]) return;
+
+    self.locationManager = [[CLLocationManager alloc]init]; //inicijalizujemo ga
+    self.locationManager.delegate = self; //mi smo njegov delegat
+    self.locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters; //na svakih 10m belezi promenu
+    [self.locationManager requestWhenInUseAuthorization]; //samo kada je u radi trazi lokaciju
+    [self.locationManager startUpdatingLocation]; //belezi znacajne promene startmonitoringsigni
+}
+
+#pragma mark - UIApplicationDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self configureLocationManager];
+    
     return YES;
 }
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    if (locations.count > 0) {
+        DataManager *dataManager = [DataManager sharedManager];
+        dataManager.userLocation = [locations lastObject];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"LOcation manager: %@", error.localizedDescription);
+}
+
 @end
---
+++ Projekat ToDo/ToDo/ToDo/DataManager.m
@@ -0,0 +1,53 @@
+//
+//  DataManager.m
+//  ToDo
+//
+//  Created by Biljana on 2/25/17.
+//  Copyright © 2017 Djuro Alfirevic. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+#pragma mark - Properties
+
+- (void)setUserLocation:(CLLocation *)userLocation {
+    _userLocation = userLocation;
+    
+    CLGeocoder *geocoder = [[CLGeocoder alloc]init];
+    [geocoder reverseGeocodeLocation:userLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"CLGeocoder error: %@", error.localizedDescription);
+        }
+        
+        if (placemarks.count > 0) {
+            CLPlacemark *placemark = [placemarks firstObject];
+            
+            self.userLocality = placemark.locality;
+            
+            NSLog(@"ISOcountryCode: %@", placemark.ISOcountryCode);
+            NSLog(@"Country: %@", placemark.country);
+            NSLog(@"Postal code: %@", placemark.postalCode);
+            NSLog(@"Locality: %@", placemark.subLocality);
+            //i ima ih jos dosta
+
+        }
+    }];
+}
+
+#pragma mark -
+
++ (instancetype)sharedManager {
+    static DataManager *shared;
+    
+    @synchronized (self) {
+        if (shared == nil) {
+            shared = [[DataManager alloc]init];
+        }
+    }
+    
+    return shared;
+}
+
+@end
